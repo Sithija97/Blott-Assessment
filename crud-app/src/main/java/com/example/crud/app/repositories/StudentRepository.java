8fe538c924303c6203b8e2b6806300f7bf5d94d5
Sithija Shehara <nsithijashehara@gmail.com>
2026-01-15 19:56:54 +0530

    initial commit

--- crud-app/src/main/java/com/example/crud/app/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.app.repositories;
-
-import com.example.crud.app.model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends MongoRepository<Student, Long>{
-}
